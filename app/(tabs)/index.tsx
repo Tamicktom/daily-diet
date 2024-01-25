fdcc1c095d293118aabbd902d42f3b025afa2bb3
Tamicktom <henrique.angelo@enterscience.com.br>
2024-01-25 02:31:44 -0300

    revamp

--- app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-//* Libraries imports
-import {  View } from 'react-native';
-
-export default function TabOneScreen() {
-  return (
-    <View className='items-center justify-center flex-1 bg-black'>
-    </View>
-  );
-}
